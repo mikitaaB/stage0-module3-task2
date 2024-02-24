ed568d95c1a15bd903f1b96cad4101fd3eb7f82b
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-24 22:04:23 +0300

    module 3. task 2. fix

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,6 +1,6 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
+public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
     private int age;
@@ -10,7 +10,7 @@ public class advancedNamingConvention {
         //some code
     }
 
-    void callByNumber (int num) {
+    void callByNumber (int number) {
         //some code
     }
 }
