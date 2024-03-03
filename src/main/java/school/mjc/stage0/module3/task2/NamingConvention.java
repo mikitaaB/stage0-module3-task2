c5e1a9bd17884e497dd90f403650120adf103a4e
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-04 00:26:46 +0300

    module 3. task 2. fix

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -3,13 +3,13 @@ package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_one = 1;
-        int variable_two = 2;
-        int variable_three = 3;
+        int variableOne = 1;
+        int variableTwo = 2;
+        int variableThree = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
-        int variable_six = 6;
-        int variable_seven = 7;
-        int variable_eight = 8;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
